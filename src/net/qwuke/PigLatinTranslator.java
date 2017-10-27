df2c3fb4f63244bcb49d9e46ed617f670651d6c9
Tristan Mortimer <tristan.e.mortimer@gmail.com>
2017-10-26 20:10:24 -0400

    Sorta got it

--- src/net/qwuke/PigLatinTranslator.java
+++ src/net/qwuke/PigLatinTranslator.java
@@ -10,36 +10,59 @@ package net.qwuke;
 import java.util.Scanner;
 
 public class PigLatinTranslator {
-	
-	
-	
-	public static String translate(String input) {
-		//Method for starting off translating the input by initially breaking up the input into a string array
-		String[] dividedInput = input.split("\\s+");
-		
-		//This is where the bulk of translating goes on where we examine individual words and work from there
-		for(String e:dividedInput) {
-			System.out.println(e);
-			
+
+	final static String vowels = "aeiouy";
+
+
+	public static int posOfFirstVowel(String word) {
+		//where we find vowels in a string for anything not starting with a vowel
+		for (int index = 0; index < word.length(); index++) {
+			if (vowels.contains(String.valueOf(word.charAt(index)))) {
+				return index;
+			}
 		}
-		
-		return "translated input";
+
+		// Index when vowels aren't found
+		return -1;
 	}
 	
-	
+
+	public static String translate(String input) {
+		// Method for starting off translating the input by initially breaking up the
+		// input into a string array
+
+		// This is where the bulk of translating goes on where we examine individual
+		// words and work from there
+		String translatedWords = "";
+
+		for (String word : input.toLowerCase().split("\\s+")) {
+			char firstChar = Character.toUpperCase(word.charAt(0));
+			if (firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u') {
+				translatedWords += (word + "way");
+			} else if (firstChar == 'q') {
+				
+			} else {
+				int i = posOfFirstVowel(word);
+				System.out.print(word.substring(i) + word.substring(0, i) + "ay");
+			}
+
+		}
+
+		return translatedWords;
+
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String valid = " "; // String for validating whether or not to continue based on y or n
 		System.out.println("Welcome to the Pig Latin Translator");
 
 		do {
-			//First we validate user input
+			// First we validate user input
 			System.out.println("Enter a line to be translated: ");
-			//method to handle 
-			
-			
-			
-			translate(sc.nextLine());//Input handling line
+			// method to handle
+
+			translate(sc.nextLine());// Input handling line
 			System.out.println();
 			System.out.println("Continue? Type y to continue or n to end the program");
 			valid = sc.nextLine();
@@ -50,5 +73,5 @@ public class PigLatinTranslator {
 		} while (valid.equals("y") || valid.equals("Y")); // If y, rerun the program
 		sc.close();
 	}
-	
+
 }
